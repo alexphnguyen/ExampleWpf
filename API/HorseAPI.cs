1f168847358ab0cb4de64190e0017890cb6bdb60
Alex <dharmaturtle@gmail.com>
2015-10-20 14:38:12 -0500

    created API

---
+++ API/HorseAPI.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+using DataModel;
+using Enity;
+using System.Linq;
+
+namespace API {
+  public class HorseAPI {
+    public List<HorseModel> GetAllHorses() {
+      return Enity.HorsesTable.Horses.Select(h => new HorseModel(h)).ToList();
+    }
+  }
+}
--- DataModel/HorseModel.cs
+++ DataModel/HorseModel.cs
@@ -1,8 +1,18 @@
-﻿
+﻿using Enity;
+
 namespace DataModel {
   public class HorseModel {
+    public HorseModel(Horse horse) {
+      Load(horse);
+    }
+
     public string Name { get; set; }
     public int Legs { get; set; }
     public bool Useful { get { return Legs == 4; } }
+
+    private void Load(Horse h) {
+      this.Name = h.Name;
+      this.Legs = h.Legs;
+    }
   }
 }
