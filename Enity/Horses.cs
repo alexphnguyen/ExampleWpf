c148899a8b7498d7a389aa04308631fbb48c785b
Alex <dharmaturtle@gmail.com>
2015-10-20 11:03:25 -0500

    added code to fake the database

---
+++ Enity/Horses.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Enity {
+  /// <summary>
+  /// Plays the role of the database with some initial data
+  /// </summary>
+  public static class Horses {
+
+    private List<Horse> _horses = new List<Horse> {
+      new Horse{Id=1, Name="Alice"},
+      new Horse{Id=2, Name="Bob"},
+      new Horse{Id=3, Name="Cryptography"}
+    };
+
+    public List<Horse> Horses {
+      get { return _horses; }
+      set { _horses = value; }
+    }
+  }
+}
