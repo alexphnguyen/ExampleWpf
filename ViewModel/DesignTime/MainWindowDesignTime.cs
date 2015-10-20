6c7fad0052040e842307a1e16556c233255c0228
Alex <dharmaturtle@gmail.com>
2015-10-20 13:21:01 -0500

    bound data to view

---
+++ ViewModel/DesignTime/MainWindowDesignTime.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ViewModel.DesignTime {
+  public class MainWindowDesignTime : MainWindowViewModel {
+    public MainWindowDesignTime() {
+      /*
+       * The purpose of these default values are to populate the design time view.
+       * They will not appear at runtime.
+       * This allows you to easily style the View,
+       * previewing things like font color,
+       * botton border size,
+       * or the apperance of a selected item in a list.
+       */
+      Name = "Default Name";
+      Legs = 0;
+      Useful = false;
+    }
+  }
+}
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,5 +1,16 @@
 ﻿
 namespace ViewModel {
+  /// <summary>
+  /// These are the values that will have "Bindings" in MainWindow.
+  /// Bindings mean that when these properties change and PropertyChanged is called,
+  /// the values in the View will also change.
+  /// </summary>
   public class MainWindowViewModel : ViewModelBase {
+    public MainWindowViewModel() {
+
+    }
+    public string Name { get; set; }
+    public int Legs { get; set; }
+    public bool Useful { get; set; }
   }
 }
