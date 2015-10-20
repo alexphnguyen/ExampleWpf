bfd708fc57a17492d61c177d4b43b33872ddec53
Alex <dharmaturtle@gmail.com>
2015-10-20 14:55:13 -0500

    added view model

---
+++ ViewModel/HorseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DataModel;
+namespace ViewModel {
+
+  /// <summary>
+  /// This layer will hold properties specific to the view,
+  /// including things like IsSelected, IsExpanded (for trees), and IsFocused.
+  /// </summary>
+  public class HorseViewModel {
+    public HorseViewModel(HorseModel hm) {
+      Model = hm;
+    }
+
+    public HorseModel Model { get; set; }
+  }
+}
