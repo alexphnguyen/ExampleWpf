f537d42ffdefaf676364384edb276acf6f3e8829
Alex Nguyen <alex.nguyen@blocherconsulting.com>
2017-07-31 13:22:28 -0500

    Deployment version number is displayed in the window title

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Deployment.Application;
+using System.Windows;
 using ViewModel;
 
 namespace WPF {
@@ -9,8 +11,22 @@ namespace WPF {
     public MainWindow() {
       InitializeComponent();
 
+      // An event that will fire when the MainWindow is loaded
+      Loaded += MainWindow_Loaded;
+
       // This connects the View with the ViewModel.
       DataContext = new MainWindowViewModel(false);
     }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+      // If this is a ClickOnce deployment, append the ClickOnce Revision to the version number (as it gets updated with each publish).
+      if (ApplicationDeployment.IsNetworkDeployed)
+        DeploymentVersion = ApplicationDeployment.CurrentDeployment.CurrentVersion;
+
+      // Display the Version as part of the window title.
+      this.Title += " Deployment Version " + DeploymentVersion;
+    }
+
+    public Version DeploymentVersion = new Version("0.0");
   }
 }
